45656f61249414d787bd9417d045feb7be49768d
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-09-23 11:22:39 +0200

    Update energyTermReflexionSimilarities.hlsl

--- Graphics/EnergyTerm/energyTermReflexionSimilarities.hlsl
+++ Graphics/EnergyTerm/energyTermReflexionSimilarities.hlsl
@@ -1,1 +1,14 @@
 
+
+
+void albedoClampForbidEnergyTerm( float albedo , ..) : SV_Target
+{
+  //the similarities here will prevent to use a common expression that enhance graphics.
+  //that said, this means we won't clamp albedo itself. We have to clamp superEnergy and 
+  //provide a compuer graphics way to keep track on it. I didn't do it with raytracing.
+ ....
+
+  //Next, we have to patch something. This will leads or nodal system to more be fast than all computer graphics
+  //patching technologies.
+  ..
+}
