6516b0866bc2c089d19329caaf47f8158fb3edc2
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-09-24 08:29:23 +0200

    Update pointLight.hlsl

--- Graphics/BTF/pointLight.hlsl
+++ Graphics/BTF/pointLight.hlsl
@@ -1,1 +1,3 @@
-
+//As point light alternatives are suited for super energy term and as this field of
+//physics deals with no weapons, this seems that this file will contain source code that
+//is like a real c or c++ program.
