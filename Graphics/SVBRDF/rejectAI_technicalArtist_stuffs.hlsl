7b1be67000a9f8eeef83ec458d5bfeb4e843a238
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-09-24 08:57:14 +0200

    Update rejectAI_technicalArtist_stuffs.hlsl

--- Graphics/SVBRDF/rejectAI_technicalArtist_stuffs.hlsl
+++ Graphics/SVBRDF/rejectAI_technicalArtist_stuffs.hlsl
@@ -1,1 +1,8 @@
-
+//As we used some kind of c code in BTF folder and as SVBRDF was found by AI,
+//there must some ways to reject AI which used no modal prototyping of graphics.
+//This seems to be a mix between super energy and high dimensional behaviours of elements.
+//That said, if we succeed to produce a c like code here, we can find a code that is not the same
+//event that occured in the past. Plus, technical arrtists stuffs are likely patching if I'm not wrong.
+//Then, super energy can be a growing field like finding some things in mathematics and translate them
+//to physics. This was done by .... (a guy who received a trophy - the first trophy for translating physics
+to maths). Finally, it means that, in fact, we won't need technical artist stuff in this folder !
