07c8fa23aa0fa03a792c4fb704f0d1e33e59a7fc
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-09-24 19:35:25 +0200

    Update forceRendering.hlsl

--- Graphics/SVBRDF/forceRendering.hlsl
+++ Graphics/SVBRDF/forceRendering.hlsl
@@ -13,6 +13,16 @@ void forceClearcoatOnlyBasicReflexion( ........ )
  //so, we can mix them and say that the ambient term is not so smart to use. Sorry, then, this means that,
 //as the ambient term is not generally an accepted thing and as this is not a model, we need rapidely a value to let 
 //propagate outgoing flux per unit surface area or unit angle area.... ? Correct me if I'm wrong ! But the idea is clear now !
+//Don t forget that texture sampling use equations. then it must a way to add a souurce code line that prevent having 
+//textures that dont deal with cos^2(x)(I did it from scratch). It is a term that I used before some more complex statements
+//in other repositories.So, it has a physical relationship with non weapons computer graphics and then log(x) for the same
+//purpose. I didn't say that I tested ii over time in this case because it comes from economy field and maybe turkish 
+//money inflation rapid studies. I used equations and functions and not models. In fact, there is no validate methods and
+//I already stated Financial rejection in other folders. Finally, it can be used in financialRejection folder too !
+
+//no texture modelling handling
+= clamp(clearcoat, cos(x)*cos(x));
+= clamp( clearcoatRoughness, cos(x)*cos(x));
 what = rand(clearcoat, clearcoatRoughness);
 return what;
 }
