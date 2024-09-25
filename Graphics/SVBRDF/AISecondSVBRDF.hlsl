f12d330f72d79d7e54c5fb1ce0283e9ebc6b0f1c
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-09-25 19:27:47 +0200

    Create AISecondSVBRDF.hlsl

---
+++ Graphics/SVBRDF/AISecondSVBRDF.hlsl
@@ -0,0 +1,18 @@
+
+//if we negate super energy here, we can have a new "weapon basics" here. We have to take advantage of my previous works
+//about AI for financial support. local models can be turned with matrix model. There must be a way and there must be
+//logics inside realtime rendering. I heavily looked at dirac works. Sorry if I'm wrong !
+
+void diracTermPreventLocalModelsForAllTextures(......)
+{
+  //the pdfs dealing with textures extraction must be fully read. As we don't have to take technical artists stuffs,
+  //it is best to create rendering algorithms or mixed rendering algorithms here. I didn't mastered textures beyond
+  //emission textures. As companies specific works deal with new textures, the "key" between all those terms may
+  //be a nuclear physics expression merged with hemisphere sampling because the raadiance or irradiance can be merged
+  //as a uniq irradiance formula ! As you can see, this means twice we won't have to give jobs to technocal artists here
+  //because CGI animation movies can have several lines of codes. Then, fortunately, as I said in a file in this folder,
+  //SVBRDF files can be c or c++ lines of code instead of pure hlsl files style.
+  //this is an esquisse now but we have then to use some extra commands that is described in my repository advanced supercomputer cell architectture.
+  //Don't kidding me ! Yes, the extra commands will be code optimization algorithms. 
+
+}
