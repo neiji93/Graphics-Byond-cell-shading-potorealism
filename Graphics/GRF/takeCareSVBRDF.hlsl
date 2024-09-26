cd875841d03466d9445b49dba696bec58b6ea64f
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-09-26 14:19:28 +0200

    Update takeCareSVBRDF.hlsl

--- Graphics/GRF/takeCareSVBRDF.hlsl
+++ Graphics/GRF/takeCareSVBRDF.hlsl
@@ -4,3 +4,25 @@
 //At the moment, we can reject all formulas we saw in mathematics in our life and produce some things !
 //I mean old approximate solutions until today ! I mean one complete approximate solution and only one !
 //My nephew Edis Louis Rasulov succeeded to produce something for himself ! Let's ask him !
+//Before that moment, I will search for nowadaays mathematical research areas and transform the statements
+//to apply in physics ! Let's try an old colleague at CNRS-LIPN chao bau Ngo !
+//I will look at extending the areas of his researches ! It seems that he doesn't share so much things.
+//So, what is he hidding ? What am I hidding ? So I choose this totally absurd sentence :
+//"Formes automorphes continues sur un espace vectorielle à 11 dimensions (matrix model) et
+//series génératrices associées". I studied generative series at college. Sorry, this is totally stupid
+//and from scratch but we can again create non weapon functions here ! Maybe it can help in other
+//areas of researches !
+
+//Let's try to start. Notice that I don't use transluciency and transparency !
+void secureAutomorpheTransmissionCommands( ..... )
+{
+  //With BSDF, there is a way to use BTDF but in the past, nobody did it.
+  //so, now have to express transmission with GRF. That means ...
+  float h = ...... ; //sorry I used something from nuclearphysicsReality folder, but physically,
+                    // this is possible to take a term that permit wide scale things with ....
+
+}
+
+
+
+
