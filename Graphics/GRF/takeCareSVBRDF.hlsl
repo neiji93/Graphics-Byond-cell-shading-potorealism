46d1bea43998e34f8b2839d0300e7e72f8f371bf
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-09-26 12:46:09 +0200

    Update takeCareSVBRDF.hlsl

--- Graphics/GRF/takeCareSVBRDF.hlsl
+++ Graphics/GRF/takeCareSVBRDF.hlsl
@@ -3,3 +3,4 @@
 //Coming soon !
 //At the moment, we can reject all formulas we saw in mathematics in our life and produce some things !
 //I mean old approximate solutions until today ! I mean one complete approximate solution and only one !
+//My nephew Edis Louis Rasulov succeeded to produce something for himself ! Let's ask him !
