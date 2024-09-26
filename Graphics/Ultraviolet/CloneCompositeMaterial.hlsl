31101da8e0d1d0d036dcb0b05982a7fdba097342
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-09-26 15:37:05 +0200

    Update CloneCompositeMaterial.hlsl

--- Graphics/Ultraviolet/CloneCompositeMaterial.hlsl
+++ Graphics/Ultraviolet/CloneCompositeMaterial.hlsl
@@ -1,1 +1,15 @@
-//
+//compositing is not so hard to understand but, fortunately, here, we have a rendering pass in result !
+//Ultraviolet folder is the way to look beyond photorealism, PBR, non photorealsim, radiosity and toon shaders !
+
+
+void fakePbrPasss(.....)
+{
+  //if you use the approximate results I had in my very older repositories
+  //you can get pi term with formulas. As this is too ugly to be an approximate
+  //result to anything in maths, we may have to use suites (work not shared) !
+  //because ... I have to remember why !
+  //But I think we can use sin function too ! This is a fake method !
+
+}
+
+
