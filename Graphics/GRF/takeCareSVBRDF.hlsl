f2fea22abd17ed574caf2955fddcbca0914dc358
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-09-26 12:45:13 +0200

    Update takeCareSVBRDF.hlsl

--- Graphics/GRF/takeCareSVBRDF.hlsl
+++ Graphics/GRF/takeCareSVBRDF.hlsl
@@ -1,1 +1,5 @@
 //as we succeed SVBRDF, we can keep track of GRF !
+//so, what we can do from scratch ? Maybe there are some ways ! Let's take my old or recent other repositories !
+//Coming soon !
+//At the moment, we can reject all formulas we saw in mathematics in our life and produce some things !
+//I mean old approximate solutions until today ! I mean one complete approximate solution and only one !
