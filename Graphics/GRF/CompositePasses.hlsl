e8d3b0eaec35625cd24a5ff404815041392dc0d0
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-09-26 16:32:43 +0200

    Create CompositePasses.hlsl

---
+++ Graphics/GRF/CompositePasses.hlsl
@@ -0,0 +1,20 @@
+//As mentionned in ultraviolet folder, we can have compositing in the ultraviolet project.
+//So, what about compositing here ? Accelerating renderer ? As our compositing functions
+//use sometimes suites, and as we used heat mechanisms, we have to create a new formula or model
+//or anything else and look if this is suited for BRF compositing ! We won't have to use physical h here
+//because we won't have to fall into models everytime ! Maybe I'm wrong in physics field here ! Let's ee what happen !
+
+
+void lastUsed_h_term_compositing(.....)
+{
+
+
+}
+
+
+//There is a logic for the name here : we used sinus .... I have to check my high school works or anything else !
+void selfRextionHeattTransferBySinus(......)
+{
+
+
+}
