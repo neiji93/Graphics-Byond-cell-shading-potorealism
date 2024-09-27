a360d975d71dc02d2ed525a48b23a833ac5b8594
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-09-27 07:25:27 +0200

    Create UVtransmissionCommandsWithPrism.hlsl

---
+++ Graphics/Ultraviolet/UVtransmissionCommandsWithPrism.hlsl
@@ -0,0 +1,18 @@
+//we already set transmission for GRF. Now, we can create transmission for ultraviolet (from scratch).
+//As my repository prism is not a weapon too, it has the advantages of separating scene into tiles like a rendering technique.
+//So, do we have to use an algorithm instead of having pure functions ? As I said, UV is not visible, so we can take
+//the transmission formulas as complete copies here because the previous formulas in GRF transmission don't have any momentum terms !
+//It means that Runge kutta 4 RK4 method is not suited here, then it means that the source code wont be too long. In physics,
+//having short formulas can be expressed in heat transfer if I'm not wrong (from scratch) ! Then, the only thing that we have to do is
+//to add some or one other formula or model ! Let me thing aabout it ! hummmm....Coming soon.
+//This is wonderfull because this approximate nothing here even it may be not secure !
+
+void secureAutomorphTransmissionDropAutomorphismToLocaalModel(...)
+{
+
+}
+
+void secureAutomorphTransmissionDropAutomorphismToOneHeatFormula(...)
+{
+
+}
