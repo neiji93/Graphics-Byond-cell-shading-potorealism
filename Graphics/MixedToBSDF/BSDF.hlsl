9a5a0e89e955d330c6a57cbaa2410e2f39dd67d6
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-09-28 16:48:18 +0200

    Update BSDF.hlsl

--- Graphics/MixedToBSDF/BSDF.hlsl
+++ Graphics/MixedToBSDF/BSDF.hlsl
@@ -25,3 +25,28 @@ void pathTracingToPrism2D(...)
 
 }
 
+//It seems that we can can mix generative series with matrix model for 11 dimensions in physics
+//So, hopefully, let s try to find a way to prevent too much textures to be hold in the rendering process with BSDF.
+void generativeSeriesToMatrixModel9(....)
+{
+
+}
+
+void hilbertSpaceToAutomorphTransmissionTerm(.....)
+{
+
+}
+
+void hilbertSpaceToAutomorphTransmissionTexturesArrayWithoutRenderingTechnique(.....)
+{
+
+}
+
+void getCosineByClosedalgebraicSpaceComplexNumbers(...)
+{
+
+}
+
+//void getCosineByClosedAlgebraaicSpaceQuaternions()
+
+//Hummmmmmm....
