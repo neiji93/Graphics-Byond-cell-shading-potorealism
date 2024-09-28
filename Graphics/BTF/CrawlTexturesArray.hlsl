ecaf8f0cdb8ae2d402b748e71112b9792817d0ef
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-09-28 08:57:44 +0200

    Create CrawlTexturesArray.hlsl

---
+++ Graphics/BTF/CrawlTexturesArray.hlsl
@@ -0,0 +1,30 @@
+
+
+//use the relationship between : 
+//1/ E * U^2 <= E * R^2 * I^2
+//and 2/ ln(2980*x) = exp(20*y)
+//see researches maths fin 2024 repository although I used that in supercomputer cells architexture
+//it helps to crawl rapidely BTF material textures
+//Maybe we can use RK4 twice here like I did for another function in another type of scattering function.
+//Hummm, it may be possible that this file helps for BTF atmospheric scattering.... Too much questions !
+//Let s design functions names first ! What is BTF from scratch and what is forbidden by scratch to be used in
+//GRF for example ? 
+//it seems the response is :  h = …
+// nombrePremier * Pi = cos( …)    ou nombrePremier * Pi = tan(…..)   No ! No ! we have to mdofiy it in physics !
+//then we have : ..... Let me check it ! 
+//see again : see researches maths fin 2024
+//Hopefully, I did researches in maths but it was not really obvious to use my researches in the supercomputer
+//repository
+
+float3 crawlRapidelyBTF(.....)
+{
+
+}
+
+//so does we have to get back in time to tangent space ?
+float3 tangentSpaceBTFForARenderingTecgnique( ... )
+{
+
+
+}
+
