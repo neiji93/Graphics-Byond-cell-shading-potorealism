10cadc59b3c73e43ec538547fafd44475a5f78e1
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-11 15:25:49 +0200

    Create LiquidMetalsToMathematics.hlsl

---
+++ Graphics/NuclearPhysicsReality/LiquidMetalsToMathematics.hlsl
@@ -0,0 +1,14 @@
+//I had difficulties to switch from physics to mathematics after the last repository futur preview.
+//It means that mathematical corpuses here hardly switch from differential functions to other differential
+//functions ! That is the first point.
+
+//in CG, differential functions can be described with BSDF but as BSDF (not itself) is hard for realtime rendering,
+//it is best to use mathematical corpus formulas. The great tool will be then to have a point light !
+//more to come.
+void commonPointLightRefreshBSDForGRF(...)
+{
+
+
+}
+
+void commonPointLightMathCorpus...
