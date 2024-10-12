9bb844cb728c5f721a81b2cad7e29e61859728bb
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-12 08:47:37 +0200

    Update TimeDependantLargeScaleMethods.hlsl

--- Graphics/ValidateModels/FlickeringAlgorithms/TimeDependantLargeScaleMethods.hlsl
+++ Graphics/ValidateModels/FlickeringAlgorithms/TimeDependantLargeScaleMethods.hlsl
@@ -6,3 +6,6 @@
 //properies. Then, turning it into physics, a great scale energy can ....
 To prove it, we may be able to use colliding atoms of liquid metals. We already know that the pathway will
 //be 0 or -1. Then, do we have to use spins ?
+//As we also worked on high numbers of terms in functions for physics of futur_preview, we can now redo that.
+//So, the problem now is log(x) that we can keep or delete from our futur equation or function. As we have 
+//a large formula, we can keep it as a constant. Then, it means our validation model can....
