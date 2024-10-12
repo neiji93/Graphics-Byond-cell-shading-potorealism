23cff622ea68e07cf0186ac89ea584200fc3f632
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-12 08:32:00 +0200

    Create TimeDependantLargeScaleMethods.hlsl

---
+++ Graphics/ValidateModels/FlickeringAlgorithms/TimeDependantLargeScaleMethods.hlsl
@@ -0,0 +1,8 @@
+
+
+//If we succeed to create flickering, it will be possible to extend the 
+//maintained time along the model to check. As we previously worked on futur_preview and
+//got 3 conductors, it seems, according to chemistry, that there are orders that govern water molecular
+//properies. Then, turning it into physics, a great scale energy can ....
+To prove it, we may be able to use colliding atoms of liquid metals. We already know that the pathway will
+//be 0 or -1. Then, do we have to use spins ?
