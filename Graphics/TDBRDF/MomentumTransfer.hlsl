85b62f3806a898996010f05e6352a81e31273b7c
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-20 08:19:58 +0100

    Update MomentumTransfer.hlsl

--- Graphics/TDBRDF/MomentumTransfer.hlsl
+++ Graphics/TDBRDF/MomentumTransfer.hlsl
@@ -2,3 +2,17 @@
 //we can test physical properties of
 //momentum transfder. but at the moment,
 //it is best to use znalyticzl solutions.
+
+
+
+/time dependant systems may be used with qunatization. This can help us having a treshold or a clamp.
+//This is usefull when we want to use transforms theorems. Then, let's use hitchin transforms,
+//as we previously in this repository (I think so), as there are relationships with riemann series.
+//Im not sure.
+void hitchinTransfformsForRiemannSeries(...)
+{
+ //We still can use computer vision theories because ...
+
+ 
+
+}
